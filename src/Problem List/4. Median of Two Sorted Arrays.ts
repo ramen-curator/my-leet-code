e499f419a165aa9a8c04f66f4a3082066783019d
赖铭志 <mz.lai@foxmail.com>
2024-06-04 14:40:54 +0800

    第四题，双测试用例

--- src/Problem List/4. Median of Two Sorted Arrays.ts
+++ src/Problem List/4. Median of Two Sorted Arrays.ts
@@ -8,6 +8,8 @@ function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
 
 const testcaseInput = [[1, 2], [3, 4]];
 const testcaseOKResult = 2.5;
+// const testcaseInput = [[1, 3], [2]];
+// const testcaseOKResult = 2;
 
 const result = findMedianSortedArrays(testcaseInput[0], testcaseInput[1]);
 myLog("result: " + result)
