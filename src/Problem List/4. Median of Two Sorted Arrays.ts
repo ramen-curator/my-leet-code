17799ecb3d6857c7bfb97309753af6478962723c
赖铭志 <mz.lai@foxmail.com>
2024-06-04 14:40:05 +0800

    初始化第四题

---
+++ src/Problem List/4. Median of Two Sorted Arrays.ts
@@ -0,0 +1,16 @@
+import { myLog } from "../util";
+
+/* Tag: Hard */
+
+function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
+    return 0;
+};
+
+const testcaseInput = [[1, 2], [3, 4]];
+const testcaseOKResult = 2.5;
+
+const result = findMedianSortedArrays(testcaseInput[0], testcaseInput[1]);
+myLog("result: " + result)
+myLog("isOK? " + (testcaseOKResult === result).toString())
+
+export default {}
