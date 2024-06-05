ab83e17a5fd7557dd21cf441c76978d8a88b4645
赖铭志 <mz.lai@foxmail.com>
2024-06-05 20:24:20 +0800

    初始化 题目五

---
+++ src/Problem List/5. Longest Palindromic Substring.ts
@@ -0,0 +1,14 @@
+import { myLog } from "../util";
+
+/* Tag:  */
+
+function longestPalindrome(s: string): string {
+    return "";
+}
+
+const testcaseInput = "babad";
+const testcaseOKResult = "bab";
+const result = longestPalindrome(testcaseInput);
+myLog("result: " + result);
+myLog("isOK? " + (testcaseOKResult === result).toString());
+export default {};
