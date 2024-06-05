b50ad88e5db8342acb7c204c4a9e7b18dd30b6b6
赖铭志 <mz.lai@foxmail.com>
2024-06-05 20:22:29 +0800

    把题目一的我的答案给复制过来

--- src/Problem List/1. Two Sum.ts
+++ src/Problem List/1. Two Sum.ts
@@ -1,1 +1,22 @@
+import { myLog } from "../util";
+
+/* Tag: Easy */
+
+function twoSum(nums: number[], target: number): number[] {
+    for (let i = 0; i < nums.length; i++) {
+        const v1 = nums[i];
+        for (let j = i + 1; j < nums.length; j++) {
+            const v2 = nums[j];
+            if (v1 + v2 === target) return [i, j];
+        }
+    }
+    return [];
+}
+
+const testcaseInput = [2, 7, 11, 15];
+const testcaseInput1 = 9;
+const testcaseOKResult = [0, 1];
+const result = twoSum(testcaseInput, testcaseInput1);
+myLog("result: " + result);
+myLog("isOK? " + (testcaseOKResult === result).toString());
 export default {};
