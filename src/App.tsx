e08cb5e6fc320358c4c620b9d4ccd834503d0fdd
赖铭志 <mz.lai@foxmail.com>
2024-06-05 20:58:57 +0800

    打出第五题

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import "./Problem List/4. Median of Two Sorted Arrays.ts";
+import "./Problem List/5. Longest Palindromic Substring.ts";
 
 function App() {
     return <div>Hello World</div>;
--- src/Problem List/5. Longest Palindromic Substring.ts
+++ src/Problem List/5. Longest Palindromic Substring.ts
@@ -1,13 +1,40 @@
 import { myLog } from "../util";
 
-/* Tag:  */
+/* Tag: Medium */
 
-function longestPalindrome(s: string): string {
-    return "";
+function longestPalindrome(str: string): string {
+    let maxArr: string[] = [];
+    for (let idx = 0; idx < str.length; idx++) {
+        let curArr: string[] = [];
+        curArr.push(str[idx]);
+        let j = 0;
+        while (str[idx] === str[idx + j + 1]) {
+            curArr.push(str[idx + j + 1]);
+            j++;
+        }
+        let ai = idx - 1,
+            bi = idx + j + 1;
+        while (ai >= 0 && bi < str.length && str[ai] === str[bi]) {
+            curArr.unshift(str[ai]);
+            curArr.push(str[bi]);
+            ai--;
+            bi++;
+        }
+        if (curArr.length > maxArr.length) {
+            maxArr = [...curArr];
+        }
+    }
+    return maxArr.join("");
 }
 
-const testcaseInput = "babad";
-const testcaseOKResult = "bab";
+// const testcaseInput = "babad";
+// const testcaseOKResult = "bab";
+// const testcaseInput = "cbbd";
+// const testcaseOKResult = "bb";
+// const testcaseInput = "ccc";
+// const testcaseOKResult = "ccc";
+const testcaseInput = "tattarrattat";
+const testcaseOKResult = "tattarrattat";
 const result = longestPalindrome(testcaseInput);
 myLog("result: " + result);
 myLog("isOK? " + (testcaseOKResult === result).toString());
