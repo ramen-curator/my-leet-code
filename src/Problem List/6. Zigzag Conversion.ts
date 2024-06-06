5c07a420f756a08aa2a9e1c07079f59da12ddb92
Curator <ramen.curator@gmail.com>
2024-06-06 08:21:14 +0800

    第六题，打了一部分

--- src/Problem List/6. Zigzag Conversion.ts
+++ src/Problem List/6. Zigzag Conversion.ts
@@ -2,7 +2,46 @@ import { myLog } from "../util";
 
 /* Tag: Medium */
 
-function convert(s: string, numRows: number): string {
+function convert(str: string, numRows: number): string {
+	let result: string[][] = [];
+	let i = 0;
+	let j = 0;
+	let k = 0;
+	let isLine = true;
+
+	for (const s of str) {
+		if (isLine) {
+			if (j < numRows) {
+				if (result[k] === undefined) result[k] = [];
+				result[k].push(s);
+				j++;
+				if (j === numRows) {
+					isLine = false;
+				}
+			} else {
+				console.error("something error");
+			}
+		} else {
+		}
+	}
+
+	while (i !== str.length) {
+		if (isLine) {
+			let tmp = [];
+			while (j < numRows) {
+				tmp.push(str[i]);
+				if (i !== str.length) {
+					i++;
+					j++;
+				}
+			}
+			result.push(tmp);
+		} else {
+			if (j !== numRows) {
+			}
+		}
+	}
+
 	return "";
 }
 const testcaseInput = "PAYPALISHIRING";
