7de43b7c7e55189d1ac07ae97036b68718a28ec0
Curator <ramen.curator@gmail.com>
2024-06-07 20:18:24 +0800

    家里的格式化的缩进跟公司的有出入

--- src/Problem List/6. Zigzag Conversion.ts
+++ src/Problem List/6. Zigzag Conversion.ts
@@ -6,58 +6,58 @@ import { myLog } from "../util";
 /* 感受：对于指针的控制水平 */
 
 function convert(str: string, numRows: number): string {
-    let resultArr: string[][] = [];
-    let j = 0;
-    let k = 0;
-    let isLine = true;
+	let resultArr: string[][] = [];
+	let j = 0;
+	let k = 0;
+	let isLine = true;
 
-    for (const s of str) {
-        if (isLine) {
-            if (j < numRows) {
-                if (resultArr[k] === undefined) resultArr[k] = [];
-                resultArr[k].push(s);
-                j++;
-                if (j === numRows) {
-                    isLine = false;
-                    j--;
-                    j--;
-                    k++;
-                }
-            } else {
-                console.error("something error");
-            }
-        } else {
-            if (j > 0) {
-                resultArr.push(new Array(numRows).fill(""));
-                resultArr[k][j] = s;
-                j--;
-                k++;
-                if (j === 0) {
-                    isLine = true;
-                }
-            } else if (j === -1) {
-                resultArr.push(new Array(numRows).fill(""));
-                j++;
-                resultArr[k][j] = s;
-                isLine = true;
-                k++;
-            } else {
-                // j===0 ，此时的numRows一定是2。
-                isLine = true;
-                if (resultArr[k] === undefined) resultArr[k] = [];
-                resultArr[k].push(s);
-                j++;
-            }
-        }
-    }
+	for (const s of str) {
+		if (isLine) {
+			if (j < numRows) {
+				if (resultArr[k] === undefined) resultArr[k] = [];
+				resultArr[k].push(s);
+				j++;
+				if (j === numRows) {
+					isLine = false;
+					j--;
+					j--;
+					k++;
+				}
+			} else {
+				console.error("something error");
+			}
+		} else {
+			if (j > 0) {
+				resultArr.push(new Array(numRows).fill(""));
+				resultArr[k][j] = s;
+				j--;
+				k++;
+				if (j === 0) {
+					isLine = true;
+				}
+			} else if (j === -1) {
+				resultArr.push(new Array(numRows).fill(""));
+				j++;
+				resultArr[k][j] = s;
+				isLine = true;
+				k++;
+			} else {
+				// j===0 ，此时的numRows一定是2。
+				isLine = true;
+				if (resultArr[k] === undefined) resultArr[k] = [];
+				resultArr[k].push(s);
+				j++;
+			}
+		}
+	}
 
-    let resultStr = "";
-    for (let i = 0; i < numRows; i++) {
-        for (let j = 0; j < resultArr.length; j++) {
-            resultStr += resultArr[j][i] ?? "";
-        }
-    }
-    return resultStr;
+	let resultStr = "";
+	for (let i = 0; i < numRows; i++) {
+		for (let j = 0; j < resultArr.length; j++) {
+			resultStr += resultArr[j][i] ?? "";
+		}
+	}
+	return resultStr;
 }
 // const testcaseInput = "PAYPALISHIRING";
 // const testcaseInput1 = 3;
