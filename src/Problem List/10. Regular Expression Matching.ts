73de685c0ace2d7f90a70526c17229eea3d8e5e4
Curator <ramen.curator@gmail.com>
2024-06-09 15:57:51 +0800

    docs: 去掉第十题多余的字符串

--- src/Problem List/10. Regular Expression Matching.ts
+++ src/Problem List/10. Regular Expression Matching.ts
@@ -18,12 +18,9 @@ function isMatch(str: string, pattern: string): boolean {
 // const testcaseInput = "aa";
 // const testcaseInput1 = "a";
 // const testcaseOKResult = false;
-// 竟然要考虑子串？
 // const testcaseInput = "aab";
 // const testcaseInput1 = "c*a*b";
 // const testcaseOKResult = true;
-// 这明明是true，怎么会是false？题目你到底什么意思？
-// 我知道了，我理解错题意了。*不是0~n个任意字符，而是0~n个任意的前一个字符。
 const testcaseInput = "mississippi";
 const testcaseInput1 = "mis*is*p*.";
 const testcaseOKResult = false;
